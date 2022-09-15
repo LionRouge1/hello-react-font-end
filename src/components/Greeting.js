4569c9f8440d4a575c9326893b694aa501810f93
Masoudi Parrot <masoudi@localhost.localdomain>
2022-09-15 05:36:57 -0400

    create greeting components

---
+++ src/components/Greeting.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getgreeting } from '../reduce/greeting/greeting';
+import './css/greeting.css';
+
+const Greeting = () => {
+  const greeting = useSelector((state) => state.greeting);
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(getgreeting());
+  };
+  return (
+    <div className="greeting">
+      <button type="button" onClick={handleClick}>Greeting</button>
+      <p>{greeting}</p>
+    </div>
+  );
+};
+
+export default Greeting;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import store from './reduce/configureStore';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
@@ -11,10 +12,12 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
